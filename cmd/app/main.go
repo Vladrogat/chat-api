02a7a4a9dceff05249fe5e6f293024dc413d7b58
Владислав Рогатых <vrogatykh@nobilis.team>
2026-01-20 15:42:15 +0300

    add application config and database

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"chat-api/internal/config"
+	"chat-api/internal/database"
+	"log"
+)
+
+func main() {
+	cfg := config.Load()
+	db, err := database.Connect(&cfg.Database)
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+}
---
+++ internal/database/database.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"chat-api/internal/config"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func Connect(cfg *config.DatabaseConfig) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(cfg.DSN()), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to connection to database: %w", err)
+	}
+
+	log.Println("Database connection successfully")
+	return db, nil
+}
